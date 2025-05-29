04b4f5db2dd41a9a90a8e452480af9eeabc1e0ba
Myron Moss <myronmoss@MacBook-Air-23.local>
2025-05-29 06:11:05 -0400

    cosmetic changes to adjust content height on screen

--- client/app/not-found.tsx
+++ client/app/not-found.tsx
@@ -6,7 +6,7 @@ import { BadgeHelp } from "lucide-react";
 
 export default function Custom404() {
   return (
-    <div className="flex items-center justify-center min-h-screen p-4">
+    <div className="flex items-center justify-center p-4">
       <Card className="w-full max-w-3xl p-8">
         <motion.div
           className="text-left"
