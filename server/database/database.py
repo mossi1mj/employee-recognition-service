9c0a565246aa40c794b135a903fa68316528ac1f
Myron Moss <myronmoss@MacBook-Air-23.local>
2025-06-05 01:49:06 -0400

    updated variables

--- server/database/database.py
+++ server/database/database.py
@@ -3,22 +3,16 @@ from dotenv import load_dotenv
 
 load_dotenv()  # load variables from .env
 
-ENVIRONMENT = os.getenv("ENVIRONMENT", "local")
+PROD_DATABASE_USER = os.getenv("DATABASE_USER")
+PROD_DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+PROD_DATABASE_HOST = os.getenv("DATABASE_HOST")
+PROD_DATABASE_PORT = os.getenv("DATABASE_PORT")
+PROD_DATABASE_NAME = os.getenv("DATABASE_NAME")
 
-if ENVIRONMENT == "production":
-    DATABASE_URL = os.getenv("PROD_DATABASE_URL")
-else:
-    # build from individual local vars
-    DATABASE_USER = os.getenv("DATABASE_USER")
-    DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
-    DATABASE_HOST = os.getenv("DATABASE_HOST")
-    DATABASE_PORT = os.getenv("DATABASE_PORT")
-    DATABASE_NAME = os.getenv("DATABASE_NAME")
-
-    DATABASE_URL = (
-        f"postgresql+asyncpg://{DATABASE_USER}:{DATABASE_PASSWORD}@"
-        f"{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
-    )
+DATABASE_URL = (
+    f"postgresql+asyncpg://{PROD_DATABASE_USER}:{PROD_DATABASE_PASSWORD}@"
+    f"{PROD_DATABASE_HOST}:{PROD_DATABASE_PORT}/{PROD_DATABASE_NAME}"
+)
 
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
